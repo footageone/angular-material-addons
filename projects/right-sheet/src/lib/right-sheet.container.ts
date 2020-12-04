3737b527e9829e5fc552c1f0ffd5059ac73cbb7f
Jason <jason@vyrotek.com>
2020-12-03 18:28:17 -0700

    ChangeDetectionStrategy updated to correctly detect changes
    Change to match official bottom-sheet ChangeDetectionStrategy.
    https://github.com/angular/components/pull/21175

--- projects/right-sheet/src/lib/right-sheet.container.ts
+++ projects/right-sheet/src/lib/right-sheet.container.ts
@@ -40,7 +40,11 @@ import { MatRightSheetConfig } from './right-sheet.config';
   selector: 'mat-right-sheet-container',
   templateUrl: './right-sheet.container.html',
   styleUrls: ['./right-sheet.container.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  // In Ivy embedded views will be change detected from their declaration place, rather than where
+  // they were stamped out. This means that we can't have the bottom sheet container be OnPush,
+  // because it might cause the sheets that were opened from a template not to be out of date.
+  // tslint:disable-next-line:validate-decorators
+  changeDetection: ChangeDetectionStrategy.Default,
   // tslint:disable-next-line: use-view-encapsulation
   encapsulation: ViewEncapsulation.None,
   animations: [matRightSheetAnimations.rightSheetState],
